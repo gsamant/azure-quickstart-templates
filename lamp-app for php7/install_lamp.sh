534650ac5cad009c050418be26fb2b0c010f7cac
Gandhali Samant <gandhalisamant@gmail.com>
2017-06-09 08:59:48 +0530

    changes echo command

--- lamp-app for php7/install_lamp.sh
+++ lamp-app for php7/install_lamp.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
-apt-get -y update
+sudo apt-get -y update
 
 # set up a silent install of MySQL
 dbpass=$1
 
 export DEBIAN_FRONTEND=noninteractive
-echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf-set-selections
-echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections
+sudo sh -c "echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf-set-selections"
+sudo sh -c "echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections"
 
 # install the LAMP stack
-apt-add-repository ppa:ondrej/php
-apt-get update
+sudo apt-add-repository ppa:ondrej/php
+sudo apt-get update
 
-apt-get -y install apache2 mysql-server php7.0 libapache2-mod-php7.0 php7.0-mysql
+sudo apt-get -y install apache2 
+sudo apt-get -y install mysql-server 
+sudo apt-get -y install php7.0
+sudo apt-get -y install php7.0-mysql
+#apt-get -y install phpmyadmin
 
 # write some PHP
-echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/phpinfo.php
-echo \<\?php phpinfo\(\)\; \?\> >> /var/www/html/phpinfo.php
+sudo sh -c "echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/phpinfo.php"
+sudo sh -c "echo \<\?php phpinfo\(\)\; \?\> >> /var/www/html/phpinfo.php"
 
 # restart Apache
-apachectl restart
+sudo service apache2 restart
